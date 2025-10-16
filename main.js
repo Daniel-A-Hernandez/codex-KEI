068eee34510acf654fb55599d62caf8b846e7ed4
Daniel Hernandez <danny20031405@gmail.com>
2025-10-16 16:46:36 -0600

    first commit-push website 80%

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-problema').addEventListener('click', ()=>{
+    document.getElementById('problema-id').scrollIntoView({behavior:'smooth'})    
+})
+
+document.getElementById('btn-solucion').addEventListener('click', ()=>{
+    document.getElementById('solucion-id').scrollIntoView({behavior:'smooth'})    
+})
+
+document.getElementById('btn-impacto').addEventListener('click', ()=>{
+    document.getElementById('impacto-id').scrollIntoView({behavior:'smooth'})    
+})
+document.getElementById('btn-move-solucion').addEventListener('click', ()=>{
+    document.getElementById('solucion-id').scrollIntoView({behavior:'smooth'})    
+})
+
+
+
