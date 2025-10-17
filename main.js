6cd5ed311fc6cd968705e9d0dba8a797a50963fe
Daniel Hernandez <danny20031405@gmail.com>
2025-10-17 02:48:26 -0600

    FIX image bugs,90%

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ document.getElementById('btn-solucion').addEventListener('click', ()=>{
     document.getElementById('solucion-id').scrollIntoView({behavior:'smooth'})    
 })
 
-document.getElementById('btn-impacto').addEventListener('click', ()=>{
-    document.getElementById('impacto-id').scrollIntoView({behavior:'smooth'})    
-})
+//document.getElementById('btn-impacto').addEventListener('click', ()=>{
+//    document.getElementById('impacto-id').scrollIntoView({behavior:'smooth'})    
+//})
+
 document.getElementById('btn-move-solucion').addEventListener('click', ()=>{
     document.getElementById('solucion-id').scrollIntoView({behavior:'smooth'})    
 })
